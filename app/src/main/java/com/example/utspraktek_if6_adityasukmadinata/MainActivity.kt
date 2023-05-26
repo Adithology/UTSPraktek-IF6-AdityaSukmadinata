3a592c9e77a75018304aff56dd4a50fa1bde2b07
Aditya <fozone088@gmail.com>
2023-05-26 18:19:02 +0700

    first commit

---
+++ app/src/main/java/com/example/utspraktek_if6_adityasukmadinata/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktek_if6_adityasukmadinata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
